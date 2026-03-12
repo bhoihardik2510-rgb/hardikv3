5ddca126c60735812037b9140a19564e79c4afae
bhoihardik2510-rgb <bhoi.hardik2510@gmail.com>
2026-03-12 21:30:45 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,20 +1,20 @@
 import os
 
 class Config(object):
-    BOT_TOKEN = "7912605698:AAF9ci_dUvTKD9A_WZcjrGD2. EDIT HERE. "
-    API_ID = 271 EDIT HERE
-    API_HASH = "e166ccc508386322f47a EDIT HERE"
-    ADMIN = '752140,59194'.split(',')
+    BOT_TOKEN = "8772401393:AAEO9WJ8ajxpwGLC1ebpg2bRwveGYwydSM4"
+    API_ID = 35437330
+    API_HASH = "d3586361e6387d8001a43d95711dbe99"
+    ADMIN = '6972231926'.split(',')
     ADMIN_ID = [int(id) for id in ADMIN]
-    DB_URL = "mongodb+srv://n8360392852:JMXqsP0puz9qwgBe@cluster0.ce0ca.mongodb EDIT HERE  "
-    DB_NAME = "WARRIOR_EXTRACTOR"
-    TXT_LOG = -10023125
-    AUTH_LOG = -10023802
-    HIT_LOG = -100247596
-    DRM_DUMP = -10024562
-    CHANNEL = -10022752
+    DB_URL = "mongodb+srv://vjbot:Hardik123@cluster0.5ykk0qp.mongodb.net/?retryWrites=true&w=majority&authSource=admin"
+    DB_NAME = "vjbot"
+    TXT_LOG = -1003574290448
+    AUTH_LOG = -1003574290448
+    HIT_LOG = -1003574290448
+    DRM_DUMP = -1003574290448
+    CHANNEL = -1003574290448
     CH_URL = "https://t.me/+vX1JFN-Mzhl"
-    OWNER = "https://t.me/mrcraacker"
+    OWNER = "https://t.me/Papainworld"
     THUMB_URL = "https://vault.pictures/media/images/b9/ad/12/b9ad123d95cd49465fcb954d.jpg" #Replace by with your Thumb URL
     HOST = "https://drm-api-five.vercel.app"
 
